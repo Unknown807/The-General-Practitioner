b2596a4f493fc1b05cdf8ea331d1f33efe55aaf9
Milovan Gveric <mg699@kent.ac.uk>
2022-02-15 14:02:06 +0000

    Project now uses maven, for mockito and junit dependencies

---
+++ src/test/java/testtemp.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class testtemp {
+    @Test
+    public void testMessage() {
+        assertEquals("message", "message");
+    }
+
+    @Test
+    public void testMessageErr() {
+        assertEquals("message", "message");
+    }
+}
