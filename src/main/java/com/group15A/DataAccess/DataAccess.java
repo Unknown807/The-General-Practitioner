167fc3a6e9223a9874cdc27de1c92044f5132811
Andrei-Constantin-Programmer <55529876+Andrei-Constantin-Programmer@users.noreply.github.com>
2022-02-17 19:10:02 +0000

    Implemented the IDataAccess in DataAccess Completed the getDoctors() method Modified: - The methods in IDataAccess now throw Exceptions - The getDoctorsTest now prints out the doctors for manual testing (WIP) - The date of birth of doctors and patients in the data model is now a Date (was previously String) - The getDoctors() method now returns the list of doctors from the database

--- src/main/java/com/group15A/DataAccess/DataAccess.java
+++ src/main/java/com/group15A/DataAccess/DataAccess.java
@@ -5,46 +5,71 @@ import com.group15A.DataModel.*;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
 
 /**
  * Used to connect to the database and query the database using stored procedures.
  *
  * @author Andrei Constantin
  */
-public class DataAccess
+public class DataAccess implements IDataAccess
 {
     private Connection connection;
 
-    public DataAccess()
+    public DataAccess() throws Exception
     {
-        try {
-            setupConnection();
-        }catch(Exception ex)
-        {
-            System.err.println(ex.getMessage());
-        }
+        setupConnection();
     }
 
-    public List<Doctor> getDoctors()
+    @Override
+    public Patient getPatient(String email, String password) throws Exception
     {
-        try{
-            String query = "CALL get_doctors();";
-            CallableStatement statement = connection.prepareCall(query);
-            //statement.setString(1, "smth");
-            ResultSet result = statement.executeQuery();
-            while(result.next())
-            {
-                System.out.println(result.getString("first_name"));
-            }
+        return null;
+    }
 
-            return new ArrayList<Doctor>();
+    @Override
+    public Patient registerPatient(Patient patient) throws Exception
+    {
+        return null;
+    }
 
-        }catch(Exception ex)
+    @Override
+    public Patient updatePatient(Patient patient) throws Exception
+    {
+        return null;
+    }
+
+    /**
+     * Get the full list of doctors from the database
+     * @return The list of doctors
+     * @throws Exception if there was a problem querying the database
+     */
+    public List<Doctor> getDoctors() throws Exception
+    {
+        String query = "CALL get_doctors();";
+        CallableStatement statement = connection.prepareCall(query);
+        //statement.setString(1, "smth");
+        ResultSet result = statement.executeQuery();
+        var doctors = new ArrayList<Doctor>();
+        while(result.next())
         {
-            System.err.println(ex.getMessage());
+            doctors.add(new Doctor(
+                    result.getInt("id_doctor"),
+                    result.getString("email"),
+                    result.getString("first_name"),
+                    result.getString("middle_name"),
+                    result.getString("last_name"),
+                    result.getDate("date_of_birth"),
+                    result.getString("gender"),
+                    result.getString("telephone_number")
+            ));
         }
 
+        return doctors;
+    }
+
+    @Override
+    public List<Certification> getCertifications(Doctor doctor) throws Exception
+    {
         return null;
     }
 
--- src/test/java/DataAccessTest.java
+++ src/test/java/DataAccessTest.java
@@ -1,19 +1,24 @@
 import com.group15A.DataAccess.DataAccess;
-import com.group15A.DataAccess.IDataAccess;
 import com.group15A.DataModel.Doctor;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
-public class DataAccessTest {
+public class DataAccessTest{
     @Test
     public void getDoctorsTest()
     {
-        var access = new DataAccess();
-        assertEquals(access.getDoctors(), new ArrayList<Doctor>());
+        //TODO Mock database for proper unit testing
+        try {
+            DataAccess access = new DataAccess();
+            var doctors = access.getDoctors();
+            for(var doctor: doctors)
+                System.out.println(doctor.getFirstName()+" "+doctor.getLastName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
