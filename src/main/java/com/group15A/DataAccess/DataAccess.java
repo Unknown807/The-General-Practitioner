6cd64caf2c60ec6cadb02f6d3541234b142ecbe2
Andrei-Constantin-Programmer <55529876+Andrei-Constantin-Programmer@users.noreply.github.com>
2022-02-17 16:30:18 +0000

    Removed: - Folder for the DataAccess Interface
    Added:
    - The DataAccess class
    - The IDataAccess interface

---
+++ src/main/java/com/group15A/DataAccess/DataAccess.java
@@ -0,0 +1,13 @@
+package com.group15A.DataAccess;
+
+/**
+ * Used to connect to the database and query the database using stored procedures.
+ *
+ * @author Andrei Constantin
+ */
+public class DataAccess
+{
+
+
+
+}
---
+++ src/main/java/com/group15A/DataAccess/IDataAccess.java
@@ -0,0 +1,11 @@
+package com.group15A.DataAccess;
+
+/**
+ * An interface to the Data Access Layer
+ *
+ * @author Andrei
+ */
+public interface IDataAccess
+{
+
+}
