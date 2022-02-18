75f30225b3bf78f4b1612b6de05ccfa75ae54a18
ww221 <ww221@kent.ac.uk>
2022-02-18 16:35:06 +0000

    create class for Doctor page for Business Logic

---
+++ src/main/java/com/group15A/BusinessLogic/DoctorLogic.java
@@ -0,0 +1,12 @@
+package com.group15A.BusinessLogic;
+
+import com.group15A.DataModel.Doctor;
+import com.group15A.IBusinessLogic.IDoctor;
+
+import java.util.ArrayList;
+
+public class DoctorLogic implements IDoctor {
+    public ArrayList<Doctor> getDoctors() {
+        return null;
+    }
+}
