d5dc86deaf7e929718a8b6b934566e0ed4515fcb
ww221 <ww221@kent.ac.uk>
2022-02-18 16:22:49 +0000

    create class for Register page for Business Logic

---
+++ src/main/java/com/group15A/BusinessLogic/RegisterLogic.java
@@ -0,0 +1,11 @@
+package com.group15A.BusinessLogic;
+
+import com.group15A.IBusinessLogic.IRegister;
+
+public class RegisterLogic implements IRegister {
+
+    public Boolean register(String email, String password) {
+        return null;
+    }
+
+}
--- src/main/java/com/group15A/IBusinessLogic/IRegister.java
+++ src/main/java/com/group15A/IBusinessLogic/IRegister.java
@@ -2,4 +2,5 @@ package com.group15A.IBusinessLogic;
 
 public interface IRegister {
     public Boolean register(String email, String password);
+
 }
