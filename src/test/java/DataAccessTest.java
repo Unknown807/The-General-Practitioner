b848793cb6d0ea4c0b713faea2495d58d814bde4
Andrei-Constantin-Programmer <55529876+Andrei-Constantin-Programmer@users.noreply.github.com>
2022-02-18 16:19:52 +0000

    Added: - getPatient method in the Data Access Layer - unit test for the getPatient method

--- src/test/java/DataAccessTest.java
+++ src/test/java/DataAccessTest.java
@@ -1,5 +1,6 @@
 import com.group15A.DataAccess.DataAccess;
 import com.group15A.DataModel.Doctor;
+import com.group15A.DataModel.Patient;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -8,17 +9,31 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class DataAccessTest{
+
+    //TODO Mock database for proper unit testing
     @Test
     public void getDoctorsTest()
     {
-        //TODO Mock database for proper unit testing
         try {
             DataAccess access = new DataAccess();
             var doctors = access.getDoctors();
             for(var doctor: doctors)
                 System.out.println(doctor.getFirstName()+" "+doctor.getLastName());
         } catch (Exception e) {
-            e.printStackTrace();
+            System.err.println("Failed to query the database.");
+        }
+    }
+
+    @Test
+    public void getPatientTest()
+    {
+        try{
+            DataAccess access = new DataAccess();
+            Patient patient = access.getPatient("superemail@hotmail.com", "superPass");
+            System.out.println(patient.getFirstName());
+        }catch (Exception ex)
+        {
+            System.err.println(ex.getMessage());
         }
     }
 }
