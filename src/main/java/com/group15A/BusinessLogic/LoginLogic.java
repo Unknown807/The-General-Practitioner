1be04075e167735c7f8425497a713a4838615d58
ww221 <ww221@kent.ac.uk>
2022-02-18 16:06:43 +0000

    create class for login page for Business Logic

---
+++ src/main/java/com/group15A/BusinessLogic/LoginLogic.java
@@ -0,0 +1,16 @@
+package com.group15A.BusinessLogic;
+
+import com.group15A.IBusinessLogic.ILogin;
+
+public class LoginLogic implements ILogin {
+
+    public Boolean login(String email, String password) {
+        return null;
+    }
+}
+
+
+
+
+
+
