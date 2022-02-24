1d4f2653210d09c872396450c2165f622ce5d5d8
Andrei-Constantin-Programmer <55529876+Andrei-Constantin-Programmer@users.noreply.github.com>
2022-02-24 21:10:50 +0000

    Code Review Custom Exceptions

--- src/main/java/com/group15A/CustomExceptions/DatabaseException.java
+++ src/main/java/com/group15A/CustomExceptions/DatabaseException.java
@@ -4,8 +4,17 @@ import com.group15A.Utils.ErrorCode;
 
 import java.util.ArrayList;
 
+/**
+ * Exception encountered when connecting to/querying the database.
+ *
+ * @author Andrei Constantin
+ */
 public class DatabaseException extends CustomException
 {
+    /**
+     * Constructor for the DatabaseException. It will create a custom exception with the DATABASE_ERROR error code.
+     * @param errorMessage The error message
+     */
     public DatabaseException(String errorMessage)
     {
         super(errorMessage, new ArrayList<>(){{add(ErrorCode.DATABASE_ERROR);}});
