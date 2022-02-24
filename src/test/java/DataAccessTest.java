be99afa0301be9bb8339fbdf308bfee08373f2bb
Andrei-Constantin-Programmer <55529876+Andrei-Constantin-Programmer@users.noreply.github.com>
2022-02-24 16:46:31 +0000

    Modified: - the DataAccessTest class: extensive modifications + removal of existing tests
    Added:
    - testCreatePatient for testing the data access layer

--- src/test/java/DataAccessTest.java
+++ src/test/java/DataAccessTest.java
@@ -1,3 +1,4 @@
+import com.group15A.CustomExceptions.DatabaseException;
 import com.group15A.DataAccess.DataAccess;
 import com.group15A.DataModel.Certification;
 import com.group15A.DataModel.Doctor;
@@ -13,76 +14,40 @@ import static org.junit.Assert.*;
 
 public class DataAccessTest extends TestCase {
 
-    //TODO Mock database for proper unit testing
-    @Test
-    public void getDoctorsTest() throws Exception {
+    private DataAccess dataAccess;
+
+    @Override
+    protected void setUp() {
         try {
-            DataAccess access = new DataAccess();
-            var doctors = access.getDoctors();
-            for(var doctor: doctors)
-                System.out.println(doctor.getFirstName()+" "+doctor.getLastName());
-        } catch (Exception ex) {
-            System.err.println("Failed to query the database.");
-            
+            super.setUp();
+        } catch (Exception e) {
+            System.err.println("Error setting up the test");
+        }
+        try {
+            dataAccess = new DataAccess();
+        } catch (DatabaseException e) {
+            System.err.println(e.getMessage());
         }
     }
 
     @Test
-    public void getPatientTest() throws Exception {
-        try{
-            DataAccess access = new DataAccess();
-            Patient patient = access.getPatient("superemail@hotmail.com", "superPass");
-            System.out.println(patient.getFirstName());
-        }catch (Exception ex)
-        {
-            System.err.println(ex.getMessage());
-            
-        }
-    }
-
-    /*@Test
-    public void updatePatientTest()
+    public void testCreatePatient()
     {
-        try{
-            DataAccess access = new DataAccess();
-            Patient patient = access.getPatient("test@test.com", "testPass");
-            patient.setMiddleName("Christopher");
-            patient = access.updatePatient(patient);
-            assertEquals(patient.getMiddleName(), access.getPatient(patient.getEmail(), patient.getPassHash()).getMiddleName());
-        }catch (Exception ex)
-        {
-            System.err.println(ex.getMessage());
-        }
-    }*/
-
-    @Test
-    public void getCertificationsTest() throws Exception {
-        try{
-            DataAccess access = new DataAccess();
-            Doctor doctor = access.getDoctors().get(0);
-            var certs = access.getCertifications(doctor);
-            for(var cert: certs)
-                System.out.println(cert.getName() + " " + cert.getField());
-        }catch (Exception ex)
-        {
-            System.err.println(ex.getMessage());
-            
-        }
-    }
-
-    @Test
-    public void changeDoctorTest() throws Exception {
         try
         {
-            DataAccess access = new DataAccess();
-            Patient patient = access.getPatient(1);
-            Doctor doctor = access.getDoctor(3);
-            System.out.println(access.getDoctor(patient).getFirstName());
-            access.changeDoctor(patient, doctor);
-            System.out.println(access.getDoctor(patient).getFirstName());
-        }catch(Exception ex)
-        {
+            Patient patient = new Patient("mynewmail3@mail.com", "myPass", "Test", null, "Testing", new Date(), "Male", "08858271");
+            Doctor doctor = dataAccess.getDoctors().get(0);
+            Patient patientFromDb = dataAccess.registerPatient(patient, doctor);
+            assertEquals(patient, patientFromDb);
+
+            var patientByEmailAndPass = dataAccess.getPatient(patient.getEmail(), patient.getPassHash());
+            assertEquals(patient, patientByEmailAndPass);
+            dataAccess.deletePatient(patientFromDb.getPatientID());
+        }catch(Exception ex) {
             System.err.println(ex.getMessage());
+            fail();
         }
     }
+
+
 }
