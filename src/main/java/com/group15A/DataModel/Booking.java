a87ee35b6f6a3f78cf73fa90b43b41af8b39b698
ww221 <ww221@kent.ac.uk>
2022-03-02 22:06:19 +0000

    Added: create the Notification class -add get and set method -equals method -hashCode method -toString

--- src/main/java/com/group15A/DataModel/Booking.java
+++ src/main/java/com/group15A/DataModel/Booking.java
@@ -1,5 +1,6 @@
 package com.group15A.DataModel;
 
+import java.time.LocalTime;
 import java.util.Date;
 import java.util.Objects;
 
@@ -9,52 +10,80 @@ import java.util.Objects;
  *
  *  @author Wenbo Wu
  */
-// class for booking patient to booking appointment
 public class Booking {
 
-    private int bookingID;
-    private int doctorID;
-    private int patientID;
-    private String bookingDate ;// the dates of booking
-    private int state;// is show booked or  cancel
+    private Integer bookingID;
+    private Integer doctorID;
+    private Integer patientID;
+    private Date bookingTime ;// the dates of booking
+    private LocalTime timestamp;// is show booked or  cancel
 
-    public int getBookingID() {
+    public Booking(Integer bookingID, Integer doctorID, Integer patientID, Date bookingTime, LocalTime timestamp) {
+        this.bookingID = bookingID;
+        this.doctorID = doctorID;
+        this.patientID = patientID;
+        this.bookingTime = bookingTime;
+        this.timestamp = timestamp;
+    }
+
+    public Integer getBookingID() {
         return bookingID;
     }
 
-    public void setBookingID(int bookingID) {
+    public void setBookingID(Integer bookingID) {
         this.bookingID = bookingID;
     }
 
-    public int getDoctorID() {
+    public Integer getDoctorID() {
         return doctorID;
     }
 
-    public void setDoctorID(int doctorID) {
+    public void setDoctorID(Integer doctorID) {
         this.doctorID = doctorID;
     }
 
-    public int getPatientID() {
+    public Integer getPatientID() {
         return patientID;
     }
 
-    public void setPatientID(int patientID) {
+    public void setPatientID(Integer patientID) {
         this.patientID = patientID;
     }
 
-    public String getBookingDate() {
-        return bookingDate;
+    public Date getBookingTime() {
+        return bookingTime;
     }
 
-    public void setBookingDate(String bookingDate) {
-        this.bookingDate = bookingDate;
+    public void setBookingTime(Date bookingTime) {
+        this.bookingTime = bookingTime;
     }
 
-    public int getState() {
-        return state;
+    public LocalTime getTimestamp() {
+        return timestamp;
     }
 
-    public void setState(int state) {
-        this.state = state;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Booking booking = (Booking) o;
+        return Objects.equals(getBookingID(), booking.getBookingID()) && Objects.equals(getDoctorID(), booking.getDoctorID()) && Objects.equals(getPatientID(), booking.getPatientID()) && Objects.equals(getBookingTime(), booking.getBookingTime()) && Objects.equals(getTimestamp(), booking.getTimestamp());
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getBookingID(), getDoctorID(), getPatientID(), getBookingTime(), getTimestamp());
+    }
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "bookingID=" + bookingID +
+                ", doctorID=" + doctorID +
+                ", patientID=" + patientID +
+                ", bookingTime=" + bookingTime +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+
 }
