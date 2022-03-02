5678a4610b16b02914e5bcce1742cc2578b1c194
ww221 <ww221@kent.ac.uk>
2022-03-02 18:42:52 +0000

    Added: create Notification class in Data Models package

---
+++ src/main/java/com/group15A/DataModel/Notification.java
@@ -0,0 +1,23 @@
+package com.group15A.DataModel;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ *  Used to represent a notification within the system, will be used to pass and change
+ *  information between the business logic and data access layer.
+ *
+ *  @author Wenbo Wu
+ */
+//class for the patient get message
+public class Notification {
+
+    private int NotificationID;
+    private int doctorID;
+    private int patientID;
+    private String Notification; // the message
+
+
+
+
+}
