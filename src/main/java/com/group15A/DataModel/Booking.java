5ea2996ad9a2ff1a50318991fbe00cf8991e13d2
ww221 <ww221@kent.ac.uk>
2022-03-02 18:42:12 +0000

    Added: create booking class in Data Models package

---
+++ src/main/java/com/group15A/DataModel/Booking.java
@@ -0,0 +1,23 @@
+package com.group15A.DataModel;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ *  Used to represent a booking within the system, will be used to pass and change
+ *  information between the business logic and data access layer.
+ *
+ *  @author Wenbo Wu
+ */
+// class for booking patient to booking appointment
+public class Booking {
+
+    private int bookingID;
+    private int doctorID;
+    private int patientID;
+    private String ;// the dates of booking
+    private int // is show booked or  cancel
+
+
+
+}
