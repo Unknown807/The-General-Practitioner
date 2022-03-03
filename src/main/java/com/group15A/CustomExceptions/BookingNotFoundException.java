5f9f82e015d18ccc9ee120ae0987802c450464ca
Andrei-Constantin-Programmer <55529876+Andrei-Constantin-Programmer@users.noreply.github.com>
2022-03-03 15:25:52 +0000

    Fixed documentation for BookingNotFoundException

--- src/main/java/com/group15A/CustomExceptions/BookingNotFoundException.java
+++ src/main/java/com/group15A/CustomExceptions/BookingNotFoundException.java
@@ -1,14 +1,14 @@
 package com.group15A.CustomExceptions;
 
 /**
- * Exception encountered when trying to get a specific doctor from the database.
+ * Exception encountered when trying to get a specific booking from the database.
  *
  * @author Andrei Constantin
  */
 public class BookingNotFoundException extends CustomException
 {
     /**
-     * Constructor for the DoctorNotFoundException. It creates a Custom Exception with no error codes and a suitable error message
+     * Constructor for the BookingNotFoundException. It creates a Custom Exception with no error codes and a suitable error message
      */
     public BookingNotFoundException()
     {
