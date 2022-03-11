028e106a9c8f6dd42769c0e59941e697f38df55f
mg699 <mg699@kent.ac.uk>
2022-03-11 21:33:09 +0000

    Code Review: ViewProfileLogic

--- src/main/java/com/group15A/BusinessLogic/ViewProfileLogic.java
+++ src/main/java/com/group15A/BusinessLogic/ViewProfileLogic.java
@@ -7,15 +7,28 @@ import com.group15A.DataModel.Doctor;
 import com.group15A.DataModel.Patient;
 
 /**
+ * Contains backend functionality for changing a patient's doctor
+ *
  * @author Milovan Gveric
  */
 public class ViewProfileLogic implements IViewProfile {
     private DataAccess dataAccessLayer;
 
+    /**
+     * Constructor for view profile logic
+     * @throws DatabaseException if issues connecting to the database
+     */
     public ViewProfileLogic() throws DatabaseException {
         this.dataAccessLayer = new DataAccess();
     }
 
+    /**
+     * Changes the patient's current doctor to a new doctor
+     * @param patientID the patient
+     * @param newDoctor the patient's new doctor
+     * @throws CustomException if issue with finding the patient, doctor, updating the patient's
+     * new doctor or when finally creating a notification
+     */
     @Override
     public void updatePatientDoctor(Integer patientID, Doctor newDoctor) throws CustomException {
         Patient patient = this.dataAccessLayer.getPatient(patientID);
