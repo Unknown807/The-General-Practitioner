d2a324fe7a80ca10d591d568c1062025c54d705e
Filip Fois <ff235@kent.ac.uk>
2022-03-15 18:34:15 +0000

    Created MessagePanel superclass for all messages to extend

---
+++ src/main/java/com/group15A/GUI/MessagePanel.java
@@ -0,0 +1,61 @@
+package com.group15A.GUI;
+
+import com.group15A.Utils.ErrorCode;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+
+/**
+ */
+public class MessagePanel
+{
+    public JPanel contentPanel;
+    public JPanel headingContentPanel;
+    public JLabel headingLabel;
+    public JLabel subheadingLabel;
+    public JTextPane messageTextPane;
+    public JButton button;
+    public JPanel messagePanel;
+
+    public MessagePanel(String heading, String subheading, String message, String buttonText)
+    {
+        HashMap<JComponent, String> widgets = new HashMap<>() {{
+            put(headingLabel, heading);
+            put(subheadingLabel, subheading);
+            put(messageTextPane, message);
+            put(button, buttonText);
+        }};
+        
+        for(JComponent component : widgets.keySet()){
+            if(widgets.get(component).equals("")){
+                component.setVisible(false);
+                contentPanel.remove(component);
+            }
+            else{
+                if(component instanceof JButton){
+                    ((JButton)component).setText(widgets.get(component));
+                }
+                else if(component instanceof JLabel){
+                    ((JLabel)component).setText(widgets.get(component));
+                }
+                else if(component instanceof JTextPane){
+                    ((JTextPane)component).setText(widgets.get(component));
+                }
+            }
+
+        }
+    }
+
+    public JPanel getMainPanel()
+    {
+        return messagePanel;
+    }
+
+    public JButton getButton()
+    {
+        return button;
+    }
+
+
+}
