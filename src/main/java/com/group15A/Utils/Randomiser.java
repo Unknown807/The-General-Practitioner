53142a082c1b535bac252fd5cf7f21c5d5f971a5
ww221 <ww221@kent.ac.uk>
2022-03-22 22:38:07 +0000

    Added initial Randomiser class structure

---
+++ src/main/java/com/group15A/Utils/Randomiser.java
@@ -0,0 +1,29 @@
+package com.group15A.Utils;
+
+import java.util.Random;
+
+public class Randomiser {
+    private final String[] prescriptions = new String[] {
+
+    };
+
+    private final String[] details = new String[] {
+
+    };
+
+    private final Integer presLength = prescriptions.length;
+    private final Integer detailsLength = details.length;
+    private Random randomGen;
+
+    public Randomiser() {
+        this.randomGen = new Random();
+    }
+
+    public String getRandPrescription() {
+
+    }
+
+    public String getRandDetails() {
+
+    }
+}
