d8385e8484469d5df2a787e69a5ae74bb6dea835
ww221 <ww221@kent.ac.uk>
2022-03-22 23:17:26 +0000

    Added method for getting the random details.

--- src/main/java/com/group15A/Utils/Randomiser.java
+++ src/main/java/com/group15A/Utils/Randomiser.java
@@ -4,11 +4,30 @@ import java.util.Random;
 
 public class Randomiser {
     private final String[] prescriptions = new String[] {
-
+            "Headache medicine",
+            "Skin Cream",
+            "Pain-killer",
+            "Oxprenolol",
+            "Cardiovascular drugs",
+            "Insulin",
+            "Cold medication",
+            "Antacids",
+            "Stomach medicine",
+            "Allergy medication",
+            "Asthma medicine",
+            "Antivirus medication",
+            "Heart disease medication",
+            "Hypertension medicine",
+            "Gout medication",
+            "Diabetes drugs",
+            "Contraception pills",
+            "Chronic disease medication",
+            "Sore throat medicine"
     };
 
     private final String[] details = new String[] {
-
+            "My weekly checkup was performed and the doctor reported that I was in good health",
+            ""
     };
 
     private Random randomGen;
