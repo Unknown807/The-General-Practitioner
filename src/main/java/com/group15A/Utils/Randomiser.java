0b46a502bf7a94a77be69341518d6f231faccd47
ww221 <ww221@kent.ac.uk>
2022-03-23 00:15:47 +0000

    Added method for randomiser  details class

--- src/main/java/com/group15A/Utils/Randomiser.java
+++ src/main/java/com/group15A/Utils/Randomiser.java
@@ -28,7 +28,19 @@ public class Randomiser {
 
     private final String[] details = new String[] {
             "My weekly checkup was performed and the doctor reported that I was in good health",
-            ""
+            "The blood sample was taken by the doctor",
+            "The prescriptions has updated by doctor, you need using new medication",
+            "For Allergy the doctor suggest to make regular check, to made sure the progression of seriously ",
+            "doctor suggest drink boiling water",
+            "doctor suggest eating low-sugar food in the daily meals, for prevent diabetes",
+            "doctor suggest eating low-salt food in the daily meals, for prevent CVD",
+            "doctor suggest eating low-fat food in the daily meals, for prevent cardiovascular disease",
+            "report form doctor examine shown I have gluten allergy, I need start eat gluten-free diet ",
+            "The bio-sample was taken by the doctor",
+            "The first vaccine has taken by the doctor and second vaccine need make a appointment",
+            "The doctor has booked XR Orthopantomogram full for the body check, I need keep an appointment",
+            "The doctor has booked surgery for my bones, I need keep an appointment",
+
     };
 
     private Random randomGen;
