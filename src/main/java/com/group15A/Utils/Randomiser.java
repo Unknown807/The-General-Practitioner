2c763b84e4493885865ebb5fd417d7aeaff0fcb4
Filip Fois <ff235@kent.ac.uk>
2022-03-24 18:20:22 +0000

    Updated array of visit detail texts and a prescription text in Randomiser.

--- src/main/java/com/group15A/Utils/Randomiser.java
+++ src/main/java/com/group15A/Utils/Randomiser.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Randomiser {
     private final String[] prescriptions = new String[] {
             "Headache medicine",
-            "Skin Cream",
+            "Skin cream",
             "Pain-killer",
             "Oxprenolol",
             "Cardiovascular drugs",
@@ -27,19 +27,19 @@ public class Randomiser {
     };
 
     private final String[] details = new String[] {
-            "My weekly checkup was performed and the doctor reported that I was in good health",
-            "The blood sample was taken by the doctor",
-            "The prescriptions has updated by doctor, you need using new medication",
-            "For Allergy the doctor suggest to make regular check, to made sure the progression of seriously ",
-            "doctor suggest drink boiling water",
-            "doctor suggest eating low-sugar food in the daily meals, for prevent diabetes",
-            "doctor suggest eating low-salt food in the daily meals, for prevent CVD",
-            "doctor suggest eating low-fat food in the daily meals, for prevent cardiovascular disease",
-            "report form doctor examine shown I have gluten allergy, I need start eat gluten-free diet ",
-            "The bio-sample was taken by the doctor",
-            "The first vaccine has taken by the doctor and second vaccine need make a appointment",
-            "The doctor has booked XR Orthopantomogram full for the body check, I need keep an appointment",
-            "The doctor has booked surgery for my bones, I need keep an appointment",
+            "Weekly checkup was performed. Patient was in good health.",
+            "Blood sample was taken.",
+            "Old prescription is no longer being recommended. New prescription assigned.",
+            "Patient found to have an allergy. Regular check-ups recommended to assess patient health.",
+            "Patient was advised to drink warm water.",
+            "Patient was advised to eat low-sugar food for daily meals, to prevent diabetes.",
+            "Patient was advised to eat low-salt food for daily meals, to prevent CVD.",
+            "Patient was advised to eat low-fat food for daily meals, to prevent cardiovascular disease.",
+            "Examination shows patient has gluten allergy. A gluten free diet was recommended.",
+            "Patient's bio-sample was taken.",
+            "Patient took first vaccine. An appointment for the second vaccination must be made.",
+            "Scheduled future full Orthopantomogram X-Ray appointment.",
+            "Scheduled future appointment for patient to have orthopedic surgery.",
 
     };
 
