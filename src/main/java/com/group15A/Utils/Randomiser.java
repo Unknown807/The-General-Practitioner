7e88cf43b53e8da00c4d3d789639fb11a18db0c0
mg699 <mg699@kent.ac.uk>
2022-03-25 15:15:08 +0000

    Code Review: Randomiser

--- src/main/java/com/group15A/Utils/Randomiser.java
+++ src/main/java/com/group15A/Utils/Randomiser.java
@@ -2,6 +2,13 @@ package com.group15A.Utils;
 
 import java.util.Random;
 
+/**
+ * Mocks doctors and their inputs regarding a patient's booking prescriptions and details
+ * (in lieu of the doctor application)
+ *
+ * @author Wenbo Wu
+ * @author Milovan Gveric
+ */
 public class Randomiser {
     private final String[] prescriptions = new String[] {
             "Headache medicine",
@@ -23,7 +30,7 @@ public class Randomiser {
             "Contraception pills",
             "Chronic disease medication",
             "Sore throat medicine",
-            "Oral Health medication"
+            "Oral health medication"
     };
 
     private final String[] details = new String[] {
@@ -38,21 +45,27 @@ public class Randomiser {
             "Examination shows patient has gluten allergy. A gluten free diet was recommended.",
             "Patient's bio-sample was taken.",
             "Patient took first vaccine. An appointment for the second vaccination must be made.",
-            "Scheduled future full Orthopantomogram X-Ray appointment.",
-            "Scheduled future appointment for patient to have orthopedic surgery.",
+            "Scheduled for future full Orthopantomogram X-Ray appointment.",
+            "Scheduled for future appointment for patient to have orthopedic surgery.",
 
     };
 
-    private Random randomGen;
+    private final Random randomGen;
 
     public Randomiser() {
         this.randomGen = new Random();
     }
 
+    /**
+     * @return a random string from the prescriptions array
+     */
     public String getRandPrescription() {
         return prescriptions[randomGen.nextInt(prescriptions.length)];
     }
 
+    /**
+     * @return a random string from the details array
+     */
     public String getRandDetails() {
         return details[randomGen.nextInt(details.length)];
     }
